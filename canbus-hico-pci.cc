f65cd25128d4b61133153988b988134670347169
vibot4_lab <vibot4_lab@vibot4-lab-pc.(none)>
2011-02-15 10:03:54 +0100

    new driver for hicocan-pci added

---
+++ canbus-hico-pci.cc
@@ -0,0 +1,262 @@
+#include "canbus-hico-pci.hh"
+#include "hicocan.h"
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <fcntl.h>
+
+#define BAUDRATE HiCOCAN_BAUD1M
+
+using namespace canbus;
+
+DriverHicoPCI::DriverHicoPCI()
+    : IODriver(sizeof(can_msg))
+    , m_read_timeout(DEFAULT_TIMEOUT)
+    , m_write_timeout(DEFAULT_TIMEOUT) {}
+
+#define SEND_IOCTL(cmd) {\
+    int ret = ioctl(fd, cmd); \
+    if (ret == -1) \
+    { \
+        perror(#cmd); \
+        return false; \
+    } \
+}
+#define SEND_IOCTL_2(cmd, arg) {\
+    int ret = ioctl(fd, cmd, arg); \
+    if (ret == -1) \
+    { \
+        perror(#cmd); \
+        return false; \
+    } \
+      
+}
+
+void DriverHicoPCI::~DriverHicoPCI()
+{ // done
+ int ret =-1;
+ int fd=getFileDescriptor(); 
+
+ ret= ioctl(fd,IOC_STOP);
+  
+
+  if (ret!=-1)
+     std::cout<<"NODE stopped"<<std::endl;
+
+  // close the node
+  close(fd);
+  
+}
+
+bool DriverHicoPCI::reset_board()
+{//done ?
+    if (!DriverHicoPCI::reset_board(m_fd))
+        return false;
+
+    /* Leave this in this order. For some reason it is more accurate */
+    timestampBase = base::Time::now();
+    int fd = m_fd; // for SEND_IOCTL
+    
+    /// this function is neccessary ?
+    //SEND_IOCTL(IOC_RESET_TIMESTAMP);
+    return true;
+}
+bool DriverHicoPCI::reset_board(int fd)
+{//done
+	ioctl(fd,IOC_RESET_BOARD);
+   // SEND_IOCTL(IOC_RESET_BOARD);
+    return true;
+}
+
+bool DriverHicoPCI::reset()
+{ //done
+    if (!DriverHicoPCI::reset(m_fd))
+        return false;
+
+    timestampBase = base::Time::fromSeconds(0);
+    return true;
+}
+bool DriverHicoPCI::reset(int fd)
+{//done
+    int bitrate = static_cast<int>(BITRATE_1000k);
+    //SEND_IOCTL_2(IOC_SET_BITRATE, &bitrate);
+    
+    int ret= ioctl(fd,IOC_STOP);
+    
+    if (ret!=-1)
+     std::cout<<"NODE stopped"<<std::endl;
+    
+    ret= ioctl(fd,IOC_START);
+    
+    std::cout<<ret<<std::endl;
+    
+    if(ret == -1)
+    {
+     printf("Problem to enable the NODES\n");
+     return false;
+    }
+    else
+     printf(" NODES ENABLED\n");
+    
+    //SEND_IOCTL(IOC_STOP);
+    //SEND_IOCTL(IOC_START);
+    return true;
+}
+
+bool DriverHicoPCI::setBaudRate(int fd, int Rate)
+{//done
+   canParam parameters;
+   
+   parameters.baud = Rate;
+   	
+   int ret=ioctl(fd, IOC_SET_BAUD, &parameters);
+   
+   if(ret==-1)
+   {
+     printf("ERROR while setting Baudrate\n");
+     return false;
+   }
+   else
+   {
+     printf("Baud Rate set\n");
+     return true;
+   }
+	
+	
+}
+
+void DriverHicoPCI::setReadTimeout(uint32_t timeout)
+{ m_read_timeout = timeout; }
+uint32_t DriverHicoPCI::getReadTimeout() const
+{ return m_read_timeout; }
+void DriverHicoPCI::setWriteTimeout(uint32_t timeout)
+{ m_write_timeout = timeout; }
+uint32_t DriverHicoPCI::getWriteTimeout() const
+{ return m_write_timeout; }
+
+bool DriverHicoPCI::open(std::string const& path)
+{ // done
+    if (isValid())
+        close();
+
+    int fd = ::open(path.c_str(), O_RDWR);
+    if (fd == INVALID_FD)
+        return false;
+    // set the baudrate
+    int ret = setBaudRate(fd, BAUDRATE);    
+        
+    if(!ret)
+        return false;  
+        
+    //deactivate filtering, activate non blocking    
+    int flags = fcntl(fd,F_GETFL);
+        
+    fcntl(fd, F_SETFL, ( flags | O_NONBLOCK));      
+        
+    file_guard guard(fd);
+    if (!reset(fd))
+        return false;
+
+    setFileDescriptor(guard.release());
+    return true;
+}
+
+Message DriverHicoPCI::read()
+{//done (have a look at the timestamp)
+    canMsg msg;
+    readPacket(reinterpret_cast<uint8_t*>(&msg), sizeof(canMsg), m_read_timeout);
+
+    Message result;
+    result.time     = base::Time::now();
+    result.can_time = base::Time::fromMicroseconds(msg.ts.us) +
+      timestampBase;
+    result.can_id        = msg.id;
+    memcpy(result.data, msg.data, 8);
+    result.size          = msg.dlc;
+    return result;
+}
+
+void DriverHicoPCI::write(Message const& msg)
+{//done
+    canMsg out;
+    memset(&out, 0, sizeof(canMsg));
+    out.id = msg.can_id;
+    out.rtr = 0;
+    out.ff = HiCOCAN_FORMAT_BASIC;
+    memcpy(out.data, msg.data, 8);
+    out.dlc   = msg.size;
+  
+    writePacket(reinterpret_cast<uint8_t*>(&out), sizeof(canMsg), m_write_timeout);
+}
+
+int DriverHicoPCI::extractPacket(uint8_t const* buffer, size_t buffer_size) const
+{ //what is this for?
+    if (buffer_size == sizeof(canMsg))
+        return sizeof(canMsg);
+    else
+        return -buffer_size;
+}
+
+int DriverHicoPCI::getPendingMessagesCount()
+{//done
+    int count = 0;
+    canState curstat;
+    int fd = getFileDescriptor();
+    
+    ioctl(fd,IOC_GET_CAN_STATE,&curstat);
+    
+    count = curstat.recBuf;
+  
+  //  ioctl(getFileDescriptor(), IOC_MSGS_IN_RXBUF, &count);
+    return count;
+}
+
+bool DriverHicoPCI::checkBusOk() 
+{//done
+  uint32_t status;
+  canState curstat;
+  
+  int fd = getFileDescriptor();
+  
+  ioctl(fd,IOC_GET_CAN_STATE,&curstat);
+  
+  status = curstat.state;
+  
+  //SEND_IOCTL_2(IOC_GET_CAN_STATUS, &status);
+
+  if((status & CSC_ERR_PSV) || 
+     (status & C_BUSOFF)){
+    return false;
+  }
+
+  return true;
+}
+
+void DriverHicoPCI::clear()
+{//done
+    int count = getPendingMessagesCount();
+    for (int i = 0; i < count; ++i)
+    {
+        canMsg msg;
+        readPacket(reinterpret_cast<uint8_t*>(&msg), sizeof(canMsg), m_read_timeout);
+    }
+}
+
+int DriverHicoPCI::getFileDescriptor() const 
+{// not touched
+    return IODriver::getFileDescriptor();
+}
+
+/** True if a valid file descriptor is assigned to this object */
+bool DriverHicoPCI::isValid() const
+{// not touched
+    return IODriver::isValid();
+}
+
+/** Closes the file descriptor */
+void DriverHicoPCI::close()
+{// not touched
+    IODriver::close();
+}
+
---
+++ canbus-hico-pci.hh
@@ -0,0 +1,136 @@
+#ifndef CANBUS_HICO_PCI_HH
+#define CANBUS_HICO_PCI_HH
+#include "canmessage.hh"
+#include "iodrivers_base.hh"
+#include "canbus.hh"
+#include <string>
+
+
+namespace canbus
+{
+    /** This class allows to (i) setup a CAN interface and (ii) having read and
+     * write access to it.
+     */
+    class DriverHicoPCI : public IODriver, public Driver
+    {
+        bool sendSetupIOCTL(std::string const& name, int cmd);
+        template<typename T>
+        bool sendSetupIOCTL(std::string const& name, int cmd, T arg);
+
+        uint32_t m_read_timeout;
+        uint32_t m_write_timeout;
+
+        base::Time timestampBase;
+
+        int extractPacket(uint8_t const* buffer, size_t buffer_size) const;
+
+    public:
+        /** The default timeout value in milliseconds
+         *
+         * @see setTimeout, getTimeout
+         */
+        static const int DEFAULT_TIMEOUT = 100;
+
+        DriverHicoPCI();
+        ~DriverHicoPCI();
+
+        /** Opens the given device and resets the CAN interface. It returns
+         * true if the initialization was successful and false otherwise
+         */
+        bool open(std::string const& path);
+
+        /** Resets the CAN board. This must be called before
+	 *  any calls to reset() on any of the interfaces of the same
+	 *  board
+         *
+         * @return false on error, true on success
+         */
+        bool reset_board();
+        /** Resets the CAN board. This must be called before
+	 *  any calls to reset() on any of the interfaces of the same
+	 *  board
+         *
+         * @return false on error, true on success
+         */
+        static bool reset_board(int fd);
+        /** Resets the CAN interface
+         *
+         * @return false on error, true on success
+         */
+        bool reset();
+        /** Resets the given CAN interface
+         *
+         * @return true on success, false on error.
+         */
+        static bool reset(int fd);
+
+        /** Sets the timeout, in milliseconds, for which we are allowed to wait
+         * for write access is write()
+         */
+        void     setWriteTimeout(uint32_t timeout);
+        /** Returns the timeout, in milliseconds, for which we are allowed to wait
+         * for packets in read() or for write access in write()
+         */
+        uint32_t getWriteTimeout() const;
+        /** Sets the timeout, in milliseconds, for which we are allowed to wait
+         * for packets in read()
+         */
+        void     setReadTimeout(uint32_t timeout);
+        /** Returns the timeout, in milliseconds, for which we are allowed to wait
+         * for packets in read() or for write access in write()
+         */
+        uint32_t getReadTimeout() const;
+
+        /** Reads the next message. It is guaranteed to not block longer than the
+         * timeout provided by setReadTimeout().
+         *
+         * The default timeout value is given by DEFAULT_TIMEOUT
+         */
+        Message read();
+
+        /** Writes a message. It is guaranteed to not block longer than the
+         * timeout provided in setWriteTimeout().
+         *
+         * The default timeout value is given by DEFAULT_TIMEOUT
+         */
+        void write(Message const& msg);
+
+        /** Returns the number of messages queued in the board's RX queue
+         */
+        int getPendingMessagesCount();
+
+      /** Checks if bus reports error, this may indicate a disconnected cable
+       *  this method will only report an error after an message was written
+       *  to the bus
+       */
+      bool checkBusOk();
+      
+
+        /** Removes all pending messages from the RX queue
+         */
+        void clear();
+
+	/** Returns the file descriptor associated with this object. If no file
+	 * descriptor is assigned, returns INVALID_FD
+	 */
+	int getFileDescriptor() const;
+
+	/** True if a valid file descriptor is assigned to this object */
+	bool isValid() const;
+
+	/** Closes the file descriptor */
+	void close();
+  private:
+    bool setBaudRate(int fd, int Rate);
+	
+public : 
+        canParam canbusParameters;
+        
+        int can_node;
+        	
+	
+    };
+}
+
+#endif
+
