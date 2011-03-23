a8122e6fc92ac52fcb599243faed309b296bbd7e
Janosch Machowinski <janosch.machowinski@dfki.de>
2011-03-23 11:40:01 +0100

    added parameter for device type to canbus reset tool

--- tools/canbus_reset.cc
+++ tools/canbus_reset.cc
@@ -4,11 +4,17 @@
 
 int main(int argc, char** argv)
 {
-    if (argc != 2)
+    if (argc != 3)
     {
-        std::cerr << "canbus_reset can_device" << std::endl;
+        std::cerr << "canbus_reset can_device device_type" << std::endl;
         return 1;
     }
-    std::auto_ptr<canbus::Driver> driver(canbus::openCanDevice(argv[1]));
+    std::auto_ptr<canbus::Driver> driver(canbus::openCanDevice(std::string(argv[1]), std::string(argv[2])));
+    
+    if(!driver.get())
+    {
+	std::cerr << "Failed to open can device of type " << argv[2] << " with path " << argv[1] << std::endl;
+	return 1;
+    }
     driver->reset_board();
 }
