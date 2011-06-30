ea9e8db11544edc0fd46d2580ce578c2717eeed2
Janosch Machowinski <janosch.machowinski@dfki.de>
2011-06-30 18:13:32 +0200

    added tool for sending can messages over the command line

---
+++ tools/send_tool.cc
@@ -0,0 +1,72 @@
+#include <iostream>
+#include "canbus.hh"
+#include <iomanip>
+#include <map>
+#include <boost/lexical_cast.hpp>
+
+
+int main(int argc, char** argv)
+{
+    if (argc < 5 || argc > 13) 
+    {
+        std::cerr
+            << "usage: canbus_send device device_type id length [value1] ... [value8]\n"
+            << std::endl;
+        return 1;
+    }
+
+    std::map<int, int> statistics;
+    canbus::Driver * driver = canbus::openCanDevice(argv[1], argv[2]);
+    if (!driver)
+        return 1;
+    if (!driver->reset())
+        return 1;
+
+    int id;
+    int lenght;
+    id = strtol(argv[3], NULL, 16);
+    if(id < 0 || id > (1<<11))
+    {
+	std::cerr << std::endl << "ID must between 0 and 2^11" << std::endl;
+	return 1;
+    }
+
+
+    lenght = strtol(argv[4], NULL, 0);
+
+    std::cout << "id: 0x" << std::hex << id << " lenght: " << lenght << " data :";
+
+    if(lenght < 0 || lenght > 8)
+    {
+	std::cerr << std::endl << "Lenght must between 0 and 8" << std::endl;
+	return 1;
+    }
+    
+    if(argc != lenght + 5)
+    {
+	std::cerr << std::endl << "Error, number of parameters does not match length" << std::endl;
+	return 1;
+    }
+
+    canbus::Message msg;
+    msg.can_id = id;
+    msg.size = lenght;
+    int tmp;
+    for(int i = 0; i < lenght; i++) 
+    {
+	tmp = strtol(argv[5 + i], NULL, 16);
+	if(tmp < 0 || tmp > 255)
+	{
+	    std::cerr << "Error, given value nr " << i << " is wrong : " << tmp << std::endl;
+	    return 1;
+	}
+	msg.data[i] = tmp;
+	std::cout << " 0x" << tmp; 
+    }
+    std::cout << std::endl;
+
+    driver->write(msg);
+
+    return 0;
+    
+}
